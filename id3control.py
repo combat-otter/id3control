6267d649ee60420e6c4def40700744ea2bcebfc1
gc <g@b.c>
2021-11-19 11:30:26 +0000

    pwd from file

--- id3control.py
+++ id3control.py
@@ -2,20 +2,26 @@ import argparse
 import json
 from weconnect import weconnect
 
+IGNORE_ITEMS = ["lvBatteryStatus", "parkingPosition", "capabilityStatus", "rangeStatus"]
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--username', required=True)
-    parser.add_argument('-p', '--password', required=True)
 
-    args = parser.parse_args()
+    with open ("config.json") as f:
+        config = json.load(f)
 
+    
     print('Connecting...')
-    weConnect = weconnect.WeConnect(username=args.username, password=args.password)
+    weConnect = weconnect.WeConnect(username=config["username"], password=config["password"])
     
     for vin, vehicle in weConnect.vehicles.items():
 
         print(vin)
-        print(vehicle.statuses)
+        
+        status = vehicle.statuses
+        for s in status:
+            if s not in IGNORE_ITEMS:
+                print(status[s])
+
     
 if __name__ == '__main__':
     main()
